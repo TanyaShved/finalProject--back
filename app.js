855d7ccc0bf7bf4edac957f237c816716ab8fc2a
Yurii Zubar <barsuk0831@gmail.com>
2021-04-16 18:08:54 +0300

    limits

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
+const helmet = require('helmet');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const swaggerUi = require('swagger-ui-express');
 
 const swaggerDocument = require('./swagger.json');
@@ -13,11 +15,26 @@ const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
+app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: 10000 }));
 app.use(express.urlencoded({ extended: false }));
 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  handler: (req, res, next) => {
+    return res.status(HttpCode.BAD_REQUEST).json({
+      status: 'error',
+      code: HttpCode.BAD_REQUEST,
+      data: 'Bad Request',
+      message: 'Too many requests, please try again later',
+    });
+  },
+});
+
+app.use('*', apiLimiter);
 app.use('/test', testRouter);
 app.use('/auth', usersRouter);
 
