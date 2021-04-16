0095aad6ffc80ff36c48643a2c8cbcacadd19dcd
Yurii Zubar <barsuk0831@gmail.com>
2021-04-16 18:16:22 +0300

    fix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json({ limit: 10000 }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const apiLimiter = rateLimit({
@@ -34,7 +34,7 @@ const apiLimiter = rateLimit({
   },
 });
 
-app.use('*', apiLimiter);
+app.use('/', apiLimiter);
 app.use('/test', testRouter);
 app.use('/auth', usersRouter);
 
